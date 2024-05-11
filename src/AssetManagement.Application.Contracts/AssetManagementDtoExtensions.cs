74ba30582c136a4e17af9f2d3ccc7f911af36dfa
Chandrashekar J <87321782+96068chandra@users.noreply.github.com>
2024-05-11 19:21:31 +0530

    improve

--- src/AssetManagement.Application.Contracts/AssetManagementDtoExtensions.cs
+++ src/AssetManagement.Application.Contracts/AssetManagementDtoExtensions.cs
@@ -1,17 +1,21 @@
-﻿using Volo.Abp.Identity;
+using System;
+using Volo.Abp.Identity;
 using Volo.Abp.ObjectExtending;
 using Volo.Abp.Threading;
 
-namespace AssetManagement;
-
-public static class AssetManagementDtoExtensions
+namespace AssetManagement
 {
-    private static readonly OneTimeRunner OneTimeRunner = new OneTimeRunner();
-
-    public static void Configure()
+    public static class AssetManagementDtoExtensions
     {
-        OneTimeRunner.Run(() =>
+        private static readonly OneTimeRunner OneTimeRunner = new OneTimeRunner();
+
+        /// <summary>
+        /// Configures extension properties for DTOs defined in the depended modules.
+        /// </summary>
+        public static void Configure()
         {
+            OneTimeRunner.Run(() =>
+            {
                 /* You can add extension properties to DTOs
                  * defined in the depended modules.
                  *
@@ -23,6 +27,7 @@ public static class AssetManagementDtoExtensions
                  * See the documentation for more:
                  * https://docs.abp.io/en/abp/latest/Object-Extensions
                  */
-        });
+            });
+        }
     }
 }
