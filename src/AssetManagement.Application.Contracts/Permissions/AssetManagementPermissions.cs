f79189e14854e86dc1fa648336e7827d0aa87df8
Chandrashekar J <87321782+96068chandra@users.noreply.github.com>
2024-05-11 19:22:41 +0530

    improve

--- src/AssetManagement.Application.Contracts/Permissions/AssetManagementPermissions.cs
+++ src/AssetManagement.Application.Contracts/Permissions/AssetManagementPermissions.cs
@@ -1,15 +1,32 @@
-﻿namespace AssetManagement.Permissions;
-
-public static class AssetManagementPermissions
+// Namespace for all permission-related classes and constants in the Asset Management system.
+namespace AssetManagement.Permissions
 {
-    public const string GroupName = "AssetManagement";
-    public static class Products
+    // A static class containing permission names for the Asset Management system.
+    // Permission names are defined as constants using a hierarchical naming convention.
+    // The hierarchy is based on a group name, followed by a category name and a permission name,
+    // separated by dots. This convention makes it easy to organize and manage permissions.
+    public static class Names
     {
-        public const string Default = GroupName + ".Products";
-        public const string Create = Default + ".Create";
-        public const string Edit = Default + ".Edit";
-        public const string Delete = Default + ".Delete";
+        // The name of the permission group for the Asset Management system.
+        public const string GroupName = "AssetManagement";
+
+        // The name of the category for product-related permissions.
+        public const string ProductsCategory = "Products";
+
+        // The name of the default permission for the Products category.
+        public static readonly string ProductsDefault = $"{GroupName}.{ProductsCategory}.Default";
+
+        // The name of the create permission for the Products category.
+        public static readonly string ProductsCreate = $"{GroupName}.{ProductsCategory}.Create";
+
+        // The name of the edit permission for the Products category.
+        public static readonly string ProductsEdit = $"{GroupName}.{ProductsCategory}.Edit";
+
+        // The name of the delete permission for the Products category.
+        public static readonly string ProductsDelete = $"{GroupName}.{ProductsCategory}.Delete";
+
+        // Add your own permission names here, following the same naming convention.
+        // For example:
+        // public static readonly string MyPermission1 = $"{GroupName}.MyPermission1";
     }
-    //Add your own permission names. Example:
-    //public const string MyPermission1 = GroupName + ".MyPermission1";
 }
