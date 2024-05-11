40f7759d132b54fbce394f57b8e964e6705b6b34
Chandrashekar J <87321782+96068chandra@users.noreply.github.com>
2024-05-11 19:21:35 +0530

    improve

--- src/AssetManagement.Blazor/Menus/AssetManagementMenuContributor.cs
+++ src/AssetManagement.Blazor/Menus/AssetManagementMenuContributor.cs
@@ -1,103 +1,116 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using AssetManagement.Localization;
-using AssetManagement.MultiTenancy;
 using Volo.Abp.Account.Localization;
 using Volo.Abp.Authorization.Permissions;
 using Volo.Abp.Identity.Blazor;
 using Volo.Abp.SettingManagement.Blazor.Menus;
 using Volo.Abp.TenantManagement.Blazor.Navigation;
 using Volo.Abp.UI.Navigation;
-using Volo.Abp.Users;
 
-namespace AssetManagement.Blazor.Menus;
-
-public class AssetManagementMenuContributor : IMenuContributor
+namespace AssetManagement.Blazor.Menus
 {
-    private readonly IConfiguration _configuration;
-
-    public AssetManagementMenuContributor(IConfiguration configuration)
+    public class AssetManagementMenuContributor : IMenuContributor
     {
-        _configuration = configuration;
-    }
+        private readonly IConfiguration _configuration;
 
-    public async Task ConfigureMenuAsync(MenuConfigurationContext context)
-    {
-        if (context.Menu.Name == StandardMenus.Main)
+        public AssetManagementMenuContributor(IConfiguration configuration)
         {
-            await ConfigureMainMenuAsync(context);
-        }
-        else if (context.Menu.Name == StandardMenus.User)
-        {
-            await ConfigureUserMenuAsync(context);
-        }
-    }
-
-    private Task ConfigureMainMenuAsync(MenuConfigurationContext context)
-    {
-        var l = context.GetLocalizer<AssetManagementResource>();
-
-        context.Menu.Items.Insert(
-            0,
-            new ApplicationMenuItem(
-                AssetManagementMenus.Home,
-                l["Menu:Home"],
-                "/",
-                icon: "fas fa-home"
-            )
-        );
-        context.Menu.AddItem(
-    new ApplicationMenuItem(
-        "Asset Management",
-        l["Menu:Asset Management"],
-        icon: "fa fa-book"
-    ).AddItem(
-        new ApplicationMenuItem(
-            "AssetManagement.Products",
-            l["Menu:Products"],
-            url: "/products"
-        )
-    )
-);
-
-
-        var administration = context.Menu.GetAdministration();
-
-        if (MultiTenancyConsts.IsEnabled)
-        {
-            administration.SetSubItemOrder(TenantManagementMenuNames.GroupName, 1);
-        }
-        else
-        {
-            administration.TryRemoveMenuItem(TenantManagementMenuNames.GroupName);
+            _configuration = configuration;
         }
 
-        administration.SetSubItemOrder(IdentityMenuNames.GroupName, 2);
-        administration.SetSubItemOrder(SettingManagementMenus.GroupName, 3);
+        public async Task ConfigureMenuAsync(MenuConfigurationContext context)
+        {
+            if (context.Menu.Name == StandardMenus.Main)
+            {
+                await ConfigureMainMenuAsync(context);
+            }
+            else if (context.Menu.Name == StandardMenus.User)
+            {
+                await ConfigureUserMenuAsync(context);
+            }
+        }
 
-        return Task.CompletedTask;
+        private void AddAssetManagementMenuItem(MenuConfigurationContext context, string url)
+        {
+            var l = context.GetLocalizer<AssetManagementResource>();
 
+            context.Menu.AddItem(
+                new ApplicationMenuItem(
+                    "AssetManagement",
+                    l["Menu:AssetManagement"],
+                    icon: "fa fa-book")
+                    .AddItem(
+                        new ApplicationMenuItem(
+                            "AssetManagement.Products",
+                            l["Menu:Products"],
+                            url: url
+                        )
+                    )
+            );
+        }
 
+        private Task ConfigureMainMenuAsync(MenuConfigurationContext context)
+        {
+            var l = context.GetLocalizer<AssetManagementResource>();
 
+            context.Menu.Items.Insert(
+                0,
+                new ApplicationMenuItem(
+                    AssetManagementMenus.Home,
+                    l["Menu:Home"],
+                    "/",
+                    icon: "fas fa-home"
+                )
+            );
 
-    }
+            AddAssetManagementMenuItem(context, "/products");
 
-    private Task ConfigureUserMenuAsync(MenuConfigurationContext context)
-    {
-        var accountStringLocalizer = context.GetLocalizer<AccountResource>();
+            var administration = context.Menu.GetAdministration();
 
-        var authServerUrl = _configuration["AuthServer:Authority"] ?? "";
+            if (MultiTenancyConsts.IsEnabled)
+            {
+                administration.SetSubItemOrder(TenantManagementMenuNames.GroupName, 1);
+            }
+            else
+            {
+                administration.TryRemoveMenuItem(TenantManagementMenuNames.GroupName);
+            }
 
-        context.Menu.AddItem(new ApplicationMenuItem(
-            "Account.Manage",
-            accountStringLocalizer["MyAccount"],
-            $"{authServerUrl.EnsureEndsWith('/')}Account/Manage?returnUrl={_configuration["App:SelfUrl"]}",
-            icon: "fa fa-cog",
-            order: 1000,
-            null).RequireAuthenticated());
+            administration.SetSubItemOrder(IdentityMenuNames.GroupName, 2);
+            administration.SetSubItemOrder(SettingManagementMenus.GroupName, 3);
 
-        return Task.CompletedTask;
+            return Task.CompletedTask;
+        }
+
+        private Task ConfigureUserMenuAsync(MenuConfigurationContext context)
+        {
+            if (!_configuration.GetSection("AuthServer").Exists())
+            {
+                return Task.CompletedTask;
+            }
+
+            var accountStringLocalizer = context.GetLocalizer<AccountResource>();
+
+            if (!_configuration.GetSection("AuthServer:Authority").Exists())
+            {
+                return Task.CompletedTask;
+            }
+
+            var authServerUrl = _configuration["AuthServer:Authority"] ?? "";
+
+            context.Menu.AddItem(new ApplicationMenuItem(
+                "Account.Manage",
+                accountStringLocalizer["MyAccount"],
+                $"{authServerUrl.EnsureEndsWith('/')}Account/Manage?returnUrl={_configuration["App:SelfUrl"]}",
+                icon: "fa fa-cog",
+                order: 1000,
+                null).RequireAuthenticated());
+
+            return Task.CompletedTask;
+        }
     }
 }
