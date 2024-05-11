d30778a4fbb47be41994590855a3ab1582197eca
Chandrashekar J <87321782+96068chandra@users.noreply.github.com>
2024-05-11 19:21:58 +0530

    improve

--- test/AssetManagement.EntityFrameworkCore.Tests/EntityFrameworkCore/Samples/SampleRepositoryTests.cs
+++ test/AssetManagement.EntityFrameworkCore.Tests/EntityFrameworkCore/Samples/SampleRepositoryTests.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Shouldly;
 using System;
 using System.Linq;
@@ -7,37 +7,26 @@ using Volo.Abp.Domain.Repositories;
 using Volo.Abp.Identity;
 using Xunit;
 
-namespace AssetManagement.EntityFrameworkCore.Samples;
-
-/* This is just an example test class.
- * Normally, you don't test ABP framework code
- * (like default AppUser repository IRepository<AppUser, Guid> here).
- * Only test your custom repository methods.
- */
-public class SampleRepositoryTests : AssetManagementEntityFrameworkCoreTestBase
+namespace AssetManagement.EntityFrameworkCore.Samples
 {
-    private readonly IRepository<IdentityUser, Guid> _appUserRepository;
-
-    public SampleRepositoryTests()
+    public class SampleRepositoryTests : AssetManagementEntityFrameworkCoreTestBase
     {
-        _appUserRepository = GetRequiredService<IRepository<IdentityUser, Guid>>();
-    }
+        private readonly IIdentityUserRepository _appUserRepository;
 
-    [Fact]
-    public async Task Should_Query_AppUser()
-    {
-        /* Need to manually start Unit Of Work because
-         * FirstOrDefaultAsync should be executed while db connection / context is available.
-         */
-        await WithUnitOfWorkAsync(async () =>
+        public SampleRepositoryTests()
         {
-                //Act
-                var adminUser = await (await _appUserRepository.GetQueryableAsync())
-                .Where(u => u.UserName == "admin")
-                .FirstOrDefaultAsync();
+            _appUserRepository = GetRequiredService<IIdentityUserRepository>();
+        }
 
-                //Assert
-                adminUser.ShouldNotBeNull();
-        });
+        [Fact]
+        public async Task Should_Query_AppUser()
+        {
+            // Act
+            var adminUser = await _appUserRepository.GetQueryableAsync()
+                .FirstOrDefaultAsync(u => u.UserName == "admin");
+
+            // Assert
+            adminUser.ShouldNotBeNull();
+        }
     }
 }
