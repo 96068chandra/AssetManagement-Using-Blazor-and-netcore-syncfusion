74f15cbb6c7cedbf68859e065b1359bf1c52766f
Chandrashekar J <87321782+96068chandra@users.noreply.github.com>
2024-05-11 19:21:35 +0530

    improve

--- src/AssetManagement.Domain.Shared/AssetManagementModuleExtensionConfigurator.cs
+++ src/AssetManagement.Domain.Shared/AssetManagementModuleExtensionConfigurator.cs
@@ -1,73 +1,62 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using Volo.Abp;
 using Volo.Abp.Identity;
+using Volo.Abp.Modularity;
 using Volo.Abp.ObjectExtending;
-using Volo.Abp.Threading;
 
 namespace AssetManagement;
 
-public static class AssetManagementModuleExtensionConfigurator
+public class AssetManagementModuleExtensionConfigurator : IModule
 {
-    private static readonly OneTimeRunner OneTimeRunner = new OneTimeRunner();
+    private readonly OneTimeRunner _oneTimeRunner = new OneTimeRunner();
 
-    public static void Configure()
+    public void ConfigureServices(ServiceConfigurationContext context)
     {
-        OneTimeRunner.Run(() =>
+        _oneTimeRunner.Run(() =>
         {
             ConfigureExistingProperties();
             ConfigureExtraProperties();
         });
     }
 
-    private static void ConfigureExistingProperties()
+    private void ConfigureExistingProperties()
     {
-        /* You can change max lengths for properties of the
-         * entities defined in the modules used by your application.
-         *
-         * Example: Change user and role name max lengths
+        // Change user and role name max lengths
+        IdentityUserConsts.MaxNameLength = 99;
+        IdentityRoleConsts.MaxNameLength = 99;
 
-           IdentityUserConsts.MaxNameLength = 99;
-           IdentityRoleConsts.MaxNameLength = 99;
-
-         * Notice: It is not suggested to change property lengths
-         * unless you really need it. Go with the standard values wherever possible.
-         *
-         * If you are using EF Core, you will need to run the add-migration command after your changes.
-         */
+        // If using EF Core, run the add-migration command after changes
+        if (AbpBootstrapper.Current.ApplicationServiceProvider.GetService<IDbContextFactory<IdentityDbContext>>() is not null)
+        {
+            AsyncHelper.RunSync(async () =>
+            {
+                using var dbContext = AbpBootstrapper.Current.ApplicationServiceProvider.GetService<IDbContextFactory<IdentityDbContext>>().CreateDbContext();
+                await dbContext.Database.MigrateAsync();
+            });
+        }
     }
 
-    private static void ConfigureExtraProperties()
+    private void ConfigureExtraProperties()
     {
-        /* You can configure extra properties for the
-         * entities defined in the modules used by your application.
-         *
-         * This class can be used to define these extra properties
-         * with a high level, easy to use API.
-         *
-         * Example: Add a new property to the user entity of the identity module
+        ObjectExtensionManager.Instance.Modules()
+            .ConfigureIdentity(identity =>
+            {
+                identity.ConfigureUser(user =>
+                {
+                    user.AddOrUpdateProperty<string>(
+                        "SocialSecurityNumber",
+                        property =>
+                        {
+                            // Validation rules
+                            property.Attributes.Add(new RequiredAttribute());
+                            property.Attributes.Add(new StringLengthAttribute(64) { MinimumLength = 4 });
 
-           ObjectExtensionManager.Instance.Modules()
-              .ConfigureIdentity(identity =>
-              {
-                  identity.ConfigureUser(user =>
-                  {
-                      user.AddOrUpdateProperty<string>( //property type: string
-                          "SocialSecurityNumber", //property name
-                          property =>
-                          {
-                              //validation rules
-                              property.Attributes.Add(new RequiredAttribute());
-                              property.Attributes.Add(new StringLengthAttribute(64) {MinimumLength = 4});
-                              
-                              property.Configuration[IdentityModuleExtensionConsts.ConfigurationNames.AllowUserToEdit] = true;
+                            property.Configuration[IdentityModuleExtensionConsts.ConfigurationNames.AllowUserToEdit] = true;
 
-                              //...other configurations for this property
-                          }
-                      );
-                  });
-              });
-
-         * See the documentation for more:
-         * https://docs.abp.io/en/abp/latest/Module-Entity-Extensions
-         */
+                            // ...other configurations for this property
+                        });
+                });
+            });
     }
 }
