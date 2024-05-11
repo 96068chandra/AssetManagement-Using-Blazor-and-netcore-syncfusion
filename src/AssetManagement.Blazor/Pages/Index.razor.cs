a0323b2e2cb9550baafa5244bcd4df2aff226415
Chandrashekar J <87321782+96068chandra@users.noreply.github.com>
2024-05-11 19:22:15 +0530

    improve

--- src/AssetManagement.Blazor/Pages/Index.razor.cs
+++ src/AssetManagement.Blazor/Pages/Index.razor.cs
@@ -1,6 +1,7 @@
-﻿namespace AssetManagement.Blazor.Pages;
-
-public partial class Index
+namespace AssetManagement.Blazor.Pages
 {
-
+    public partial class Index : ComponentBase
+    {
+        // Add any necessary properties, methods, or event handlers here.
+    }
 }
