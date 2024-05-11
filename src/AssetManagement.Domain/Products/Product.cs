572ae245cfe9c27da20087e59a5372a37d70841e
Chandrashekar J <87321782+96068chandra@users.noreply.github.com>
2024-05-11 19:22:33 +0530

    improve

--- src/AssetManagement.Domain/Products/Product.cs
+++ src/AssetManagement.Domain/Products/Product.cs
@@ -1,22 +1,80 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities;
 using Volo.Abp.Domain.Entities.Auditing;
 
 namespace AssetManagement.Products
 {
-    public class Product: AuditedAggregateRoot<Guid>
+    public class Product : AuditedEntity<Guid>
     {
-        public string Name { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; private set; }
 
-        public string Description { get; set; }
+        [Required]
+        [MaxLength(500)]
+        public string Description { get; private set; }
 
-        public float Price { get; set; }
+        [Required]
+        [Range(0, float.MaxValue)]
+        public float Price { get; private set; }
 
-        public ProductType productType { get; set; }
+        [Required]
+        public ProductType ProductType { get; private set; }
 
-        
+        public Product(Guid id, string name, string description, float price, ProductType productType)
+            : base(id)
+        {
+            SetName(name);
+            SetDescription(description);
+            SetPrice(price);
+            SetProductType(productType);
+        }
+
+        public void SetName(string name)
+        {
+            if (name.Length > 100)
+            {
+                throw new ArgumentException("Name cannot be longer than 100 characters.");
+            }
+
+            Name = name;
+        }
+
+        public void SetDescription(string description)
+        {
+            if (description.Length > 500)
+            {
+                throw new ArgumentException("Description cannot be longer than 500 characters.");
+            }
+
+            Description = description;
+        }
+
+        public void SetPrice(float price)
+        {
+            if (price < 0)
+            {
+                throw new ArgumentException("Price cannot be negative.");
+            }
+
+            Price = price;
+        }
+
+        public void SetProductType(ProductType productType)
+        {
+            ProductType = productType;
+        }
+    }
+
+    public enum ProductType
+    {
+        Electronic,
+        Mechanical,
+        Other
     }
 }
