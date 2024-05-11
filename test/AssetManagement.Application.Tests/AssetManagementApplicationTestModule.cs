e9d97183023c8570c11c961fc484e6b88e189424
Chandrashekar J <87321782+96068chandra@users.noreply.github.com>
2024-05-11 19:21:59 +0530

    improve

--- test/AssetManagement.Application.Tests/AssetManagementApplicationTestModule.cs
+++ test/AssetManagement.Application.Tests/AssetManagementApplicationTestModule.cs
@@ -1,12 +1,21 @@
-﻿using Volo.Abp.Modularity;
+using Volo.Abp.Modularity;
+using AssetManagement.Application.Contracts;
+using AssetManagement.Application.Services;
+using AssetManagement.Domain.Entities;
+using AssetManagement.Domain.Repositories;
+using Moq;
 
-namespace AssetManagement;
+namespace AssetManagement.Application.Tests;
 
 [DependsOn(
     typeof(AssetManagementApplicationModule),
     typeof(AssetManagementDomainTestModule)
-    )]
+)]
 public class AssetManagementApplicationTestModule : AbpModule
 {
-
+    public override void Initialize()
+    {
+        IocManager.Register<IMock<IAssetAppService>, Mock<IAssetAppService>>();
+        IocManager.Register<IMock<IDeviceRepository>, Mock<IDeviceRepository>>();
+    }
 }
