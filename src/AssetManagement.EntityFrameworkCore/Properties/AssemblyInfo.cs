e25c69776b71f1afe7fdb0861a152cc7241eac58
Chandrashekar J <87321782+96068chandra@users.noreply.github.com>
2024-05-11 19:22:15 +0530

    improve

--- src/AssetManagement.EntityFrameworkCore/Properties/AssemblyInfo.cs
+++ src/AssetManagement.EntityFrameworkCore/Properties/AssemblyInfo.cs
@@ -1,2 +1,23 @@
-﻿using System.Runtime.CompilerServices;
-[assembly:InternalsVisibleToAttribute("AssetManagement.EntityFrameworkCore.Tests")]
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("AssetManagement.EntityFrameworkCore.Tests")]
+
+// Only make internal types and members visible to testing projects if they are needed.
+// This reduces the risk of testing code inadvertently relying on implementation details.
+internal class InternalType
+{
+    internal void InternalMethod()
+    {
+        // Method implementation here.
+    }
+}
+
+// If internal types or members need to be accessed from external code, consider using the Internationalization Attribute.
+[assembly: InternalsVisibleTo("OtherAssemblyName")]
+internal class AnotherInternalType
+{
+    internal void AnotherInternalMethod()
+    {
+        // Method implementation here.
+    }
+}
