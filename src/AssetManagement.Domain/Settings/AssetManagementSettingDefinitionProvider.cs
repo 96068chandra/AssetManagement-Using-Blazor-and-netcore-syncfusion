685ec6d59e6b44a5c9d4e1be4fe215c7ceb05f28
Chandrashekar J <87321782+96068chandra@users.noreply.github.com>
2024-05-11 19:21:51 +0530

    improve

--- src/AssetManagement.Domain/Settings/AssetManagementSettingDefinitionProvider.cs
+++ src/AssetManagement.Domain/Settings/AssetManagementSettingDefinitionProvider.cs
@@ -1,4 +1,21 @@
-﻿using Volo.Abp.Settings;
+using Volo.Abp.Modularity;
+using Volo.Abp.Settings;
+
+namespace AssetManagement;
+
+[DependsOn(
+    typeof(AbpSettingsModule)
+)]
+public class AssetManagementSettingsModule : AbpModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        Configure<AbpSettingDefinitionProviderOptions>(options =>
+        {
+            options.Providers.Add<AssetManagementSettingDefinitionProvider>();
+        });
+    }
+}
 
 namespace AssetManagement.Settings;
 
@@ -8,5 +25,11 @@ public class AssetManagementSettingDefinitionProvider : SettingDefinitionProvide
     {
         //Define your own settings here. Example:
         //context.Add(new SettingDefinition(AssetManagementSettings.MySetting1));
+        context.Add(new SettingDefinition(AssetManagementSettings.MySetting1, "Default value", scoped: true));
     }
+}
+
+public static class AssetManagementSettings
+{
+    public const string MySetting1 = "AssetManagement:MySetting1";
 }
