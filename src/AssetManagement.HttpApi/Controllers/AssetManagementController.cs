d49466f72f3af61dd833e5ada547ab4088386e0d
Chandrashekar J <87321782+96068chandra@users.noreply.github.com>
2024-05-11 19:22:11 +0530

    improve

--- src/AssetManagement.HttpApi/Controllers/AssetManagementController.cs
+++ src/AssetManagement.HttpApi/Controllers/AssetManagementController.cs
@@ -1,14 +1,29 @@
-﻿using AssetManagement.Localization;
+using AssetManagement.Localization;
 using Volo.Abp.AspNetCore.Mvc;
+using Volo.Abp.Auditing;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Localization;
 
 namespace AssetManagement.Controllers;
 
 /* Inherit your controllers from this class.
  */
-public abstract class AssetManagementController : AbpControllerBase
+public abstract class AssetManagementController : AbpController, ITransientDependency
 {
     protected AssetManagementController()
     {
         LocalizationResource = typeof(AssetManagementResource);
+    }
+
+    protected override void AddValidationRules(CustomValidationContext context)
+    {
+        base.AddValidationRules(context);
+
+        context.AddRules(GetValidationRules());
+    }
+
+    protected virtual List<IValidationRule> GetValidationRules()
+    {
+        return new List<IValidationRule>();
     }
 }
