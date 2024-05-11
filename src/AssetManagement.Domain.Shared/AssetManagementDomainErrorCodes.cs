5c70ca71b274adc337ad8ef13fbfbf94dce3e4dc
Chandrashekar J <87321782+96068chandra@users.noreply.github.com>
2024-05-11 19:21:30 +0530

    improve

--- src/AssetManagement.Domain.Shared/AssetManagementDomainErrorCodes.cs
+++ src/AssetManagement.Domain.Shared/AssetManagementDomainErrorCodes.cs
@@ -1,6 +1,18 @@
-﻿namespace AssetManagement;
-
-public static class AssetManagementDomainErrorCodes
+namespace AssetManagement
 {
-    /* You can add your business exception error codes here, as constants */
+    public static class AssetManagementDomainErrorCodes
+    {
+        // Asset related errors
+        public const int AssetAlreadyExists = 1001;
+        public const int AssetNotFound = 1002;
+        public const int AssetCheckoutFailed = 1003;
+        public const int AssetCheckinFailed = 1004;
+
+        // User related errors
+        public const int UserNotFound = 2001;
+        public const int UserNotAuthorized = 2002;
+
+        // General errors
+        public const int UnknownError = 9999;
+    }
 }
