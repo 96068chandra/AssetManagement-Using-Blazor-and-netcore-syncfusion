63924a5609abc6da9e13bfc5b2fb708f43d46d40
Chandrashekar J <87321782+96068chandra@users.noreply.github.com>
2024-05-11 19:21:50 +0530

    improve

--- src/AssetManagement.Domain/Data/IAssetManagementDbSchemaMigrator.cs
+++ src/AssetManagement.Domain/Data/IAssetManagementDbSchemaMigrator.cs
@@ -1,8 +1,30 @@
-﻿using System.Threading.Tasks;
+using System;
+using System.Threading.Tasks;
 
-namespace AssetManagement.Data;
-
-public interface IAssetManagementDbSchemaMigrator
+namespace AssetManagement.Data
 {
-    Task MigrateAsync();
+    public interface IAssetManagementDbSchemaMigrator
+    {
+        Task MigrateAsync();
+    }
+
+    public class AssetManagementDbSchemaMigrator : IAssetManagementDbSchemaMigrator
+    {
+        private readonly string _connectionString;
+
+        public AssetManagementDbSchemaMigrator(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public async Task MigrateAsync()
+        {
+            // Implementation to migrate the database schema goes here.
+            // For example, using an ORM like Entity Framework Core:
+            using (var dbContext = new AssetManagementDbContext(_connectionString))
+            {
+                await dbContext.Database.MigrateAsync();
+            }
+        }
+    }
 }
