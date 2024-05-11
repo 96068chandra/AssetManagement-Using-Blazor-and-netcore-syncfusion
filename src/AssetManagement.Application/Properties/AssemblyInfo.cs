e5ac6d2e1708da2c97af2f44468ee8c16173dfec
Chandrashekar J <87321782+96068chandra@users.noreply.github.com>
2024-05-11 19:21:29 +0530

    improve

--- src/AssetManagement.Application/Properties/AssemblyInfo.cs
+++ src/AssetManagement.Application/Properties/AssemblyInfo.cs
@@ -1,2 +1,9 @@
-﻿using System.Runtime.CompilerServices;
-[assembly:InternalsVisibleToAttribute("AssetManagement.Application.Tests")]
+using System.Runtime.CompilerServices;
+
+// The InternalsVisibleTo attribute makes the internal members of the current assembly visible to the specified assembly.
+// This is useful when you want to allow unit tests to access internal members of your application code.
+// In this example, the "AssetManagement.Application.Tests" assembly will be able to access the internal members of the current assembly.
+[assembly: InternalsVisibleTo("AssetManagement.Application.Tests")]
+
+// You can also specify multiple assemblies to grant access to, by separating them with a semicolon.
+// [assembly: InternalsVisibleTo("Assembly1;Assembly2;Assembly3")]
