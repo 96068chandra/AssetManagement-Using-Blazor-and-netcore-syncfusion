4bfc1cc65b90935d441f12d49223dcd6b2cf0a48
Chandrashekar J <87321782+96068chandra@users.noreply.github.com>
2024-05-11 19:21:45 +0530

    improve

--- src/AssetManagement.Application.Contracts/Products/IProductAppService.cs
+++ src/AssetManagement.Application.Contracts/Products/IProductAppService.cs
@@ -1,13 +1,24 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using Volo.Abp;
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
 
 namespace AssetManagement.Products
 {
-    public interface IProductAppService:ICrudAppService<ProductDto,Guid,PagedAndSortedResultRequestDto,CreataUpdateProductDto>
+    public class ProductAppService : CrudAppService<Product, ProductDto, Guid, PagedAndSortedResultRequestDto, CreateUpdateProductDto>, IProductAppService
     {
+        public ProductAppService(IRepository<Product, Guid> repository) : base(repository)
+        {
+        }
 
+        protected override async Task<IQueryable<Product>> CreateFilteredQueryAsync(PagedAndSortedResultRequestDto input)
+        {
+            return (await base.CreateFilteredQueryAsync(input))
+                .WhereIf(!input.Sorting.IsNullOrWhiteSpace(), p => p.Name.Contains(input.Sorting));
+        }
     }
 }
