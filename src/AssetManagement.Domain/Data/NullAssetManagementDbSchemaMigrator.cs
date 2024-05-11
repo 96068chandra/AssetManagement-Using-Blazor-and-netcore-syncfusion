0ca773fc41163a348f8c75c3ab01d5a81f7e9f5e
Chandrashekar J <87321782+96068chandra@users.noreply.github.com>
2024-05-11 19:22:01 +0530

    improve

--- src/AssetManagement.Domain/Data/NullAssetManagementDbSchemaMigrator.cs
+++ src/AssetManagement.Domain/Data/NullAssetManagementDbSchemaMigrator.cs
@@ -1,15 +1,25 @@
-﻿using System.Threading.Tasks;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using Volo.Abp.DependencyInjection;
 
 namespace AssetManagement.Data;
 
-/* This is used if database provider does't define
- * IAssetManagementDbSchemaMigrator implementation.
+/* This is used if the database provider does not define
+ * an IAssetManagementDbSchemaMigrator implementation.
  */
 public class NullAssetManagementDbSchemaMigrator : IAssetManagementDbSchemaMigrator, ITransientDependency
 {
-    public Task MigrateAsync()
+    private readonly ILogger<NullAssetManagementDbSchemaMigrator> _logger;
+
+    public NullAssetManagementDbSchemaMigrator(ILogger<NullAssetManagementDbSchemaMigrator> logger)
     {
-        return Task.CompletedTask;
+        _logger = logger;
+    }
+
+    public async Task MigrateAsync()
+    {
+        _logger.LogInformation("No database provider-specific implementation of IAssetManagementDbSchemaMigrator was found. Using the null migrator.");
+        // Perform any necessary null operations here, if needed.
+        await Task.CompletedTask;
     }
 }
