79ae90b13b3b34fe9d714dc3d749e571e42e35ac
Chandrashekar J <87321782+96068chandra@users.noreply.github.com>
2024-05-11 19:21:41 +0530

    improve

--- src/AssetManagement.Domain.Shared/MultiTenancy/MultiTenancyConsts.cs
+++ src/AssetManagement.Domain.Shared/MultiTenancy/MultiTenancyConsts.cs
@@ -1,10 +1,13 @@
-﻿namespace AssetManagement.MultiTenancy;
+namespace AssetManagement.MultiTenancy;
 
-public static class MultiTenancyConsts
+/// <summary>
+/// Contains constants related to multi-tenancy configuration.
+/// </summary>
+public static class MultiTenancySettings
 {
     /* Enable/disable multi-tenancy easily in a single point.
      * If you will never need to multi-tenancy, you can remove
      * related modules and code parts, including this file.
      */
-    public const bool IsEnabled = true;
+    public const bool IsMultiTenancyEnabled = true;
 }
