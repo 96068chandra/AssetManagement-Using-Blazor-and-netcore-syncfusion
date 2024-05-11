3fb4707017383d7422bef655f6abb3673dca4b58
Chandrashekar J <87321782+96068chandra@users.noreply.github.com>
2024-05-11 19:22:23 +0530

    improve

--- src/AssetManagement.Domain.Shared/AssetManagementGlobalFeatureConfigurator.cs
+++ src/AssetManagement.Domain.Shared/AssetManagementGlobalFeatureConfigurator.cs
@@ -1,22 +1,50 @@
-﻿using Volo.Abp.Threading;
+using Volo.Abp.FeatureManagement;
+using Volo.Abp.Modularity;
+using Volo.Abp.Threading;
 
 namespace AssetManagement;
 
-public static class AssetManagementGlobalFeatureConfigurator
+[DependsOn(
+    typeof(AbpFeatureManagementModule)
+)]
+public class AssetManagementGlobalFeatureConfigurator : AbpModule
 {
     private static readonly OneTimeRunner OneTimeRunner = new OneTimeRunner();
 
-    public static void Configure()
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        Configure<AbpFeatureManagementOptions>(options =>
+        {
+            options.Features.Add(
+                new FeatureDescription(
+                    AssetManagementGlobalFeatureNames.MyGlobalFeature,
+                    "My sample global feature's description",
+                    isEnabled: true // You can enable/disable the feature here
+                )
+            );
+        });
+
+        base.ConfigureServices(context);
+    }
+
+    public override void PostConfigureServices(ServiceConfigurationContext context)
     {
         OneTimeRunner.Run(() =>
         {
-                /* You can configure (enable/disable) global features of the used modules here.
-                 *
-                 * YOU CAN SAFELY DELETE THIS CLASS AND REMOVE ITS USAGES IF YOU DON'T NEED TO IT!
-                 *
-                 * Please refer to the documentation to lear more about the Global Features System:
-                 * https://docs.abp.io/en/abp/latest/Global-Features
-                 */
+            /* You can configure (enable/disable) global features of the used modules here.
+             *
+             * Please refer to the documentation to lear more about the Global Features System:
+             * https://docs.abp.io/en/abp/latest/Global-Features
+             */
+
+            FeatureManager.SetFeatureEnabled(AssetManagementGlobalFeatureNames.MyGlobalFeature, true);
         });
+
+        base.PostConfigureServices(context);
     }
+}
+
+public static class AssetManagementGlobalFeatureNames
+{
+    public const string MyGlobalFeature = "AssetManagement.MyGlobalFeature";
 }
