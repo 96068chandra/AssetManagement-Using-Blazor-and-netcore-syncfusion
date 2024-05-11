5ee21b61c8a0ee6fe52113b9be66744add283fea
Chandrashekar J <87321782+96068chandra@users.noreply.github.com>
2024-05-11 19:21:59 +0530

    improve

--- test/AssetManagement.HttpApi.Client.ConsoleTestApp/Program.cs
+++ test/AssetManagement.HttpApi.Client.ConsoleTestApp/Program.cs
@@ -1,7 +1,11 @@
-﻿using System.Threading.Tasks;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace AssetManagement.HttpApi.Client.ConsoleTestApp;
 
@@ -9,14 +13,46 @@ class Program
 {
     static async Task Main(string[] args)
     {
-        await CreateHostBuilder(args).RunConsoleAsync();
+        try
+        {
+            var host = CreateHostBuilder(args).Build();
+            await host.RunAsync();
+        }
+        catch (Exception ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"An error occurred: {ex.Message}");
+            Console.ResetColor();
+        }
     }
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
-            .AddAppSettingsSecretsJson()
+            .ConfigureAppConfiguration((hostingContext, configuration) =>
+            {
+                configuration.Sources.Clear();
+
+                var env = hostingContext.HostingEnvironment;
+
+                configuration
+                    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+
+                configuration.AddEnvironmentVariables();
+
+                if (args != null)
+                {
+                    configuration.AddCommandLine(args);
+                }
+            })
             .ConfigureServices((hostContext, services) =>
             {
                 services.AddHostedService<ConsoleTestAppHostedService>();
+            })
+            .ConfigureLogging((hostingContext, configLogging) =>
+            {
+                configLogging.AddConsole();
+                configLogging.AddDebug();
             });
 }
+
