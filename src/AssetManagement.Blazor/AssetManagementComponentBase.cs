7361a74f080cdd3b704580e9836d4bc182fbeea5
Chandrashekar J <87321782+96068chandra@users.noreply.github.com>
2024-05-11 19:21:34 +0530

    improve

--- src/AssetManagement.Blazor/AssetManagementComponentBase.cs
+++ src/AssetManagement.Blazor/AssetManagementComponentBase.cs
@@ -1,4 +1,4 @@
-﻿using AssetManagement.Localization;
+using AssetManagement.Localization;
 using Volo.Abp.AspNetCore.Components;
 
 namespace AssetManagement.Blazor;
@@ -7,6 +7,14 @@ public abstract class AssetManagementComponentBase : AbpComponentBase
 {
     protected AssetManagementComponentBase()
     {
+        // Set the localization resource for this component base
+        LocalizationResource = typeof(AssetManagementResource);
+    }
+
+    // Override the OnParametersSet method to ensure that the localization resource is set properly
+    protected override void OnParametersSet()
+    {
+        base.OnParametersSet();
         LocalizationResource = typeof(AssetManagementResource);
     }
 }
