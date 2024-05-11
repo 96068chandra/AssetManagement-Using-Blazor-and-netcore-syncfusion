6c339c8d31b06abd9d352970f0963e889fa7ed1d
Chandrashekar J <87321782+96068chandra@users.noreply.github.com>
2024-05-11 19:22:10 +0530

    improve

--- src/AssetManagement.Domain/Properties/AssemblyInfo.cs
+++ src/AssetManagement.Domain/Properties/AssemblyInfo.cs
@@ -1,3 +1,16 @@
-﻿using System.Runtime.CompilerServices;
-[assembly:InternalsVisibleToAttribute("AssetManagement.Domain.Tests")]
-[assembly:InternalsVisibleToAttribute("AssetManagement.TestBase")]
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("AssetManagement.Domain.Tests")]
+[assembly: InternalsVisibleTo("AssetManagement.TestBase")]
+
+// Add a region to group related attributes
+#region Assembly Attributes
+
+// The following attribute specifies the default culture for this assembly.
+[assembly: NeutralResourcesLanguage("en-US")]
+
+// The following attribute specifies the version information for an assembly
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+#endregion
