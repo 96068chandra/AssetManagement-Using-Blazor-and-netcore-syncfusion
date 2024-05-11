d1c9ea1bee7937b224dfcb214bfaf64750d94b2a
Chandrashekar J <87321782+96068chandra@users.noreply.github.com>
2024-05-11 19:22:18 +0530

    improve

--- src/AssetManagement.EntityFrameworkCore/EntityFrameworkCore/AssetManagementEntityFrameworkCoreModule.cs
+++ src/AssetManagement.EntityFrameworkCore/EntityFrameworkCore/AssetManagementEntityFrameworkCoreModule.cs
@@ -1,6 +1,8 @@
-﻿using System;
+using System;
+using System.Linq;
 using Microsoft.Extensions.DependencyInjection;
-using Volo.Abp.Uow;
+using Microsoft.Extensions.Options;
+using Volo.Abp;
 using Volo.Abp.AuditLogging.EntityFrameworkCore;
 using Volo.Abp.BackgroundJobs.EntityFrameworkCore;
 using Volo.Abp.EntityFrameworkCore;
@@ -13,42 +15,46 @@ using Volo.Abp.PermissionManagement.EntityFrameworkCore;
 using Volo.Abp.SettingManagement.EntityFrameworkCore;
 using Volo.Abp.TenantManagement.EntityFrameworkCore;
 
-namespace AssetManagement.EntityFrameworkCore;
-
-[DependsOn(
-    typeof(AssetManagementDomainModule),
-    typeof(AbpIdentityEntityFrameworkCoreModule),
-    typeof(AbpOpenIddictEntityFrameworkCoreModule),
-    typeof(AbpPermissionManagementEntityFrameworkCoreModule),
-    typeof(AbpSettingManagementEntityFrameworkCoreModule),
-    typeof(AbpEntityFrameworkCoreSqlServerModule),
-    typeof(AbpBackgroundJobsEntityFrameworkCoreModule),
-    typeof(AbpAuditLoggingEntityFrameworkCoreModule),
-    typeof(AbpTenantManagementEntityFrameworkCoreModule),
-    typeof(AbpFeatureManagementEntityFrameworkCoreModule)
-    )]
-public class AssetManagementEntityFrameworkCoreModule : AbpModule
+namespace AssetManagement.EntityFrameworkCore
 {
-    public override void PreConfigureServices(ServiceConfigurationContext context)
+    [DependsOn(
+        typeof(AbpAuditLoggingEntityFrameworkCoreModule),
+        typeof(AbpBackgroundJobsEntityFrameworkCoreModule),
+        typeof(AbpFeatureManagementEntityFrameworkCoreModule),
+        typeof(AbpIdentityEntityFrameworkCoreModule),
+        typeof(AbpOpenIddictEntityFrameworkCoreModule),
+        typeof(AbpPermissionManagementEntityFrameworkCoreModule),
+        typeof(AbpSettingManagementEntityFrameworkCoreModule),
+        typeof(AbpTenantManagementEntityFrameworkCoreModule),
+        typeof(AbpEntityFrameworkCoreSqlServerModule),
+        typeof(AssetManagementDomainModule))]
+    public class AssetManagementEntityFrameworkCoreModule : AbpModule
     {
-        AssetManagementEfCoreEntityExtensionMappings.Configure();
-    }
-
-    public override void ConfigureServices(ServiceConfigurationContext context)
-    {
-        context.Services.AddAbpDbContext<AssetManagementDbContext>(options =>
+        public override void PreConfigureServices(ServiceConfigurationContext context)
         {
+            AssetManagementEfCoreEntityExtensionMappings.Configure();
+        }
+
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            context.Services.AddAbpDbContext<AssetManagementDbContext>(options =>
+            {
                 /* Remove "includeAllEntities: true" to create
                  * default repositories only for aggregate roots */
-            options.AddDefaultRepositories(includeAllEntities: true);
-        });
+                options.AddDefaultRepositories(includeAllEntities: true);
+            });
 
-        Configure<AbpDbContextOptions>(options =>
-        {
+            Configure<AbpDbConnectionOptions>(options =>
+            {
+                options.ConnectionString = Configuration.GetConnectionString("Default");
+            });
+
+            Configure<AbpDbContextOptions>(options =>
+            {
                 /* The main point to change your DBMS.
                  * See also AssetManagementMigrationsDbContextFactory for EF Core tooling. */
-            options.UseSqlServer();
-        });
-
+                options.UseSqlServer();
+            });
+        }
     }
 }
