7d1d40215966780e8eae742f6505d8a7931ab55f
Chandrashekar J <87321782+96068chandra@users.noreply.github.com>
2024-05-11 19:21:33 +0530

    improve

--- src/AssetManagement.Application.Contracts/AssetManagementApplicationContractsModule.cs
+++ src/AssetManagement.Application.Contracts/AssetManagementApplicationContractsModule.cs
@@ -1,28 +1,34 @@
-﻿using Volo.Abp.Account;
+using Volo.Abp.Account;
 using Volo.Abp.FeatureManagement;
 using Volo.Abp.Identity;
 using Volo.Abp.Modularity;
-using Volo.Abp.ObjectExtending;
 using Volo.Abp.PermissionManagement;
 using Volo.Abp.SettingManagement;
 using Volo.Abp.TenantManagement;
+using Volo.Abp.Application.Services;
+using Volo.Abp.ObjectExtending;
 
 namespace AssetManagement;
 
 [DependsOn(
     typeof(AssetManagementDomainSharedModule),
-    typeof(AbpAccountApplicationContractsModule),
-    typeof(AbpFeatureManagementApplicationContractsModule),
-    typeof(AbpIdentityApplicationContractsModule),
-    typeof(AbpPermissionManagementApplicationContractsModule),
-    typeof(AbpSettingManagementApplicationContractsModule),
-    typeof(AbpTenantManagementApplicationContractsModule),
+    typeof(AbpAccountApplicationModule),
+    typeof(AbpFeatureManagementApplicationModule),
+    typeof(AbpIdentityApplicationModule),
+    typeof(AbpPermissionManagementApplicationModule),
+    typeof(AbpSettingManagementApplicationModule),
+    typeof(AbpTenantManagementApplicationModule),
     typeof(AbpObjectExtendingModule)
 )]
 public class AssetManagementApplicationContractsModule : AbpModule
 {
-    public override void PreConfigureServices(ServiceConfigurationContext context)
+    public override void ConfigureServices(ServiceConfigurationContext context)
     {
+        Configure<AbpApplicationServiceOptions>(options =>
+        {
+            options.UseAuthenticate = true;
+        });
+
         AssetManagementDtoExtensions.Configure();
     }
 }
